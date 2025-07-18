2ea39827d684e19c463146db6fb705c75eb1776c
ISAsxm <47738803+ISAsxm@users.noreply.github.com>
2025-07-18 11:27:51 -0400

    fix(cosmetics creation): removed table_args and added unique constraint in column brand_name

--- src/app/models/cosmetic.py
+++ src/app/models/cosmetic.py
@@ -4,12 +4,11 @@ from app.database.base_class import Base
 
 class Cosmetic(Base):
     __tablename__ = "cosmetics"
-    __table_args__ = {'extend_existing': True}
-
+    
     id = Column(Integer, primary_key=True, index=True)
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-    brand_name = Column(String, nullable=False)
+    brand_name = Column(String, unique=True, index=True, nullable=False)
     is_vegan = Column(Boolean, default=False)
     is_cruelty_free = Column(Boolean, default=False)
     description = Column(Text)
