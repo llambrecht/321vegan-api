bddbd971c8d236a9b5c0ee307536b5ff633cd25a
ISAsxm <47738803+ISAsxm@users.noreply.github.com>
2025-07-18 18:50:44 -0400

    feat(products): added route to fetch one by ean

--- src/app/crud/product.py
+++ src/app/crud/product.py
@@ -1,4 +1,21 @@
+from typing import Optional
+from sqlalchemy.orm import Session
 from app.crud.base import CRUDRepository
 from app.models.product import Product
 
-product_crud = CRUDRepository(model=Product)
+class ProductCRUDRepository(CRUDRepository):
+    def get_product_by_ean(self, db: Session, ean: str) -> Optional[Product]:
+        """
+        Get a product by ean.
+
+        Parameters:
+            db (Session): The database session.
+            ean (str): The ean of the product.
+
+        Returns:
+            Optional[Product]: The product found by ean, or None if not found.
+        """
+        return self.get_one(db, self._model.ean == ean)
+
+product_crud = ProductCRUDRepository(model=Product)
+
